aca688e1146448d77690736c47df651b28fb5fcb
massimiliano.corsini <massimiliano@corsini.cloud>
2023-10-06 23:10:42 +0200

    Sistemato il path di bootstrap sul json e mod al css di calcolatore-presenze.component.css

--- src/app/calcolatore-presenze/calcolatore-presenze.component.ts
+++ src/app/calcolatore-presenze/calcolatore-presenze.component.ts
@@ -6,15 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./calcolatore-presenze.component.css']
 })
 export class CalcolatorePresenzeComponent {
-  oreLezione: number = 0;
-  assenze: number = 0; // Inizializza assenze a 0
-  presenze: number = 0; // Inizializza presenze a 0
+  oreLezione!: number;
+  assenze!: number;
+  presenze!: number;
   percentualePresenze: string | null = null;
   percentualeAssenze: string | null = null;
 
   calcolaPercentuali() {
     if (this.oreLezione > 0) {
-      this.percentualePresenze = ((this.presenze / this.oreLezione) * 100).toFixed(2); // Arrotonda a 2 decimali
+      this.percentualePresenze = ((this.presenze / this.oreLezione) * 100).toFixed(2);
       this.percentualeAssenze = ((this.assenze / this.oreLezione) * 100).toFixed(2); // Arrotonda a 2 decimali
     } else {
       this.percentualePresenze = null;
